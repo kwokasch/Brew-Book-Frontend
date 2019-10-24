5d187d1735008cc9c3ce2d17d9881cc072516f34
github username <github email address>
2019-10-24 12:50:50 -0600

    add auth

--- app.js
+++ app.js
@@ -1,25 +1,53 @@
 document.addEventListener('DOMContentLoaded', () => {
     const addUser = document.querySelector('#add-user')
-
+    
     addUser.addEventListener("submit", (event) => {
         event.preventDefault()
+        
         const formData = new FormData(event.target)
 
-        fetch(`http://localhost:3000/users`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                username: formData.get("username"),
-                password: formData.get("password")
+        if (event.target.className = "signup-submit") {
+
+            fetch(`http://localhost:3000/users`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    username: formData.get("username"),
+                    password: formData.get("password")
+                })
+            }).then(response => response.json())
+            .then(response => {
+                if (!response.error){
+                    logInUser()
+                } else {
+                    console.log(response.error)
+                }
             })
-        }).then(response => response.json())
-        .then(response => {
-            console.log(response)
-            localStorage.setItem("user_id", response.id)
-            console.log(localStorage.getItem("user_id", response.id))
-            window.location.href="beer.html"
-        })
+        }
+
+        if (event.target.className = "login-submit") {
+            logInUser()
+        }
+
+        function logInUser () {
+            fetch(`http://localhost:3000/auth/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    username: formData.get("username"),
+                    password: formData.get("password")
+                })
+            }).then(response => response.json())
+            .then(response => {
+                localStorage.setItem("token", response.token)
+                localStorage.setItem("user_id", response.id)
+                window.location.href="beer.html"
+            })
+        }
+
     })
 })
